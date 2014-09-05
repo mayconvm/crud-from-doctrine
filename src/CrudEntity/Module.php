74a287d7107b0b8eea4c1f4cb44c042e3d0acf9e
Maycon Vinicius Moreira <mayconvm@gmail.com>
2014-09-04 22:27:14 -0300

    Adicionando arquivo de configuração para o módulo.

---
+++ src/CrudEntity/Module.php
@@ -0,0 +1,70 @@
+<?php
+namespace CrudEntity;
+
+use Zend\Console\Adapter\AdapterInterface as ConsoleAdapterInterface;
+use Zend\EventManager\EventInterface;
+use Zend\Mvc\ModuleRouteListener;
+use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class Module implements ConsoleUsageProviderInterface, AutoloaderProviderInterface, ConfigProviderInterface
+{
+    const NAME    = 'CrudEntity';
+
+    /**
+     * @var ServiceLocatorInterface
+     */
+    protected $sm;
+
+    public function onBootstrap(EventInterface $e)
+    {
+        $this->sm = $e->getApplication()->getServiceManager();
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConsoleBanner(ConsoleAdapterInterface $console)
+    {
+        return self::NAME;
+    }
+
+    public function getConsoleUsage(ConsoleAdapterInterface $console)
+    {
+        $config = $this->sm->get('config');
+        if (!empty($config['CrudEntity']) && !empty($config['CrudEntity']['disable_usage'])) {
+            return null; // usage information has been disabled
+        }
+
+        // TODO: Load strings from a translation container
+        return array(
+
+            'Basic information:',
+            'modules [list]'              => 'show loaded modules',
+            'version | --version'         => 'display current Zend Framework version',
+
+            'Diagnostics',
+            'diag [options] [module name]'  => 'run diagnostics',
+            array('[module name]'               , '(Optional) name of module to test'),
+            array('-v --verbose'                , 'Display detailed information.'),
+            array('-b --break'                  , 'Stop testing on first failure'),
+            array('-q --quiet'                  , 'Do not display any output unless an error occurs.'),
+            array('--debug'                     , 'Display raw debug info from tests.'),
+        );
+    }
+}
